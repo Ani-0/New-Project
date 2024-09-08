16b1ba92695e68f13989583f8fae23f346a47399
Aniket <aniketparida158@gmail.com>
2024-09-08 19:51:22 +0530

    Question 21 to 26

--- Programs.js
+++ Programs.js
@@ -1,60 +1,61 @@
-// Question 11.
-function getFahrenheit(Celsius){
-  Celsius = (Celsius * 9/5) + 32;
-  return Celsius;
+// Question 21.
+function addName(str) {
+	if (str.startsWith('py')) {
+		return str;
+	} else {
+		return 'py' + str;
+	}
 }
-console.log(getFahrenheit(60));
-//Question 13.
-// const Name = prompt('Enter your Name');
-// console.log(Name);
-// Question 15.
-function diffrenceNum (Num){
-  if(Num <= 13){
-    return Num - 13;
-  }else{
-    return (Num - 13) * 2
-  }
-};
-console.log(diffrenceNum(24));
-// Question 16.
-function sumNumber(FirstNum, SecondNum){
-   if(FirstNum === SecondNum){
-      return 3 * FirstNum + SecondNum;
-   }else{
-    return FirstNum + SecondNum;
-   }
-};
-console.log(sumNumber(10, 10));
-//Question 17.
-const specifiedNumber=(number)=>{
-  const Num = 19;
-  if(number >= Num){
-    return 3 * Num - number;
-  }else{
-    return Num - number;
-  }
-};
-console.log(specifiedNumber(22));
-// Question 18;
-const checkPair = (num1, num2)=>{
-  if((num1 == 50 || num2 == 50) || (num1 + num2 == 50)){
-    return true;
-  }else{
-    return false;
-  }
+console.log(addName('thon'));
+// Question 22.
+let first = 'Aniket';
+let second = first.slice(1);
+console.log(second);
+function getWord(letter1, num) {
+	if (letter1 < 0 || num.length) {
+		return 'invalid position';
+	} else {
+		return letter1.slice(0, num) + letter1.slice(num + 1);
+	}
 }
-console.log(checkPair(20, 20));
-//Question 19.
-function getNum(num){
-  return (Math.abs(num - 100) <= 20 || Math.abs(num - 400) <= 20);
+console.log(getWord('MichelStarc', 0));
+// Question 23.
+function getStr(Word) {
+	if (Word.length === 1) {
+		return Word;
+	}
+	const firstChar = Word[0];
+	const lastChar = Word[Word.length - 1];
+	const middelChar = Word.slice(1, -1);
+	return lastChar + middelChar + firstChar;
 }
-console.log(getNum(70));
-//Question 20.
-function getInput(a, b){
-  if ((a > 0 && b < 0) || (a < 0 && b > 0)){
-    return true;
-  }else{
-    return false;
+console.log(getStr('Mahadev'));
+//Question 24.
+function takeCharecter(letters){
+    if(letters === 0){
+      throw new Error ('String length must be 1 or more.');
+    }
+    const fastChar = letters.charAt(0);
+    return fastChar + letters + fastChar;
+}
+console.log(takeCharecter('Hello'));
+// Question 25.
+function getNum(element){
+  if(element > 0){
+    if((element % 3 === 0) || (element % 7 === 0)){
+     return true;
+    }else{
+      return false;
+    }
   }
 };
-console.log(getInput(2,-2));
+console.log(getNum(15));
+// Question 26.
+function lastThree(letters1){
+  if(letters1 < 3){
+    throw new Error ('String length must be 0 or more')
+  }
+  const lastThreeChers = letters1.substring(letters1.length -3);
+  return lastThreeChers + letters1 + lastThreeChers;
+};
+console.log(lastThree('tomorrow'))
